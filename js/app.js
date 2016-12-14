947900088e05d4b7bdd5119616232aee440033dc
Ruben Martinez <rub3nmv@gmail.com>
2016-12-14 03:37:04 +0100

    Improved visual style, failsafe code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 /*global window, console, angular*/
-var app = angular.module('startpageApp', []);
+
+var app = angular.module('startpageApp', ['ngAnimate'])
+	.config([
+		'$compileProvider',
+		function ($compileProvider) {
+			$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|chrome-extension):/);
+			$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|chrome-extension):/);
+		}
+	]);
 app.factory('weatherService', ['$http', '$q',
 	function ($http, $q) {
 		'use strict';
-
 		function getWeather(location) {
-			console.log(location);
 			var deferred = $q.defer(),
 				yql = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + location + "') and u='c'",
 				query = "https://query.yahooapis.com/v1/public/yql?q=" + yql + "&format=json&env=store://datatables.org/Falltableswithkeys";
 
 			$http.get(query).success(function (data) {
-				deferred.resolve(data.query.results.channel);
+				deferred.resolve(data.query.results);
 			}).error(function (err) {
 				console.log('Error retrieving markets');
 				deferred.reject(err);
@@ -23,45 +29,121 @@ app.factory('weatherService', ['$http', '$q',
 		};
 	}
 ]);
-app.controller('weatherController', ['$scope', 'weatherService',
-	function ($scope, weatherService) {
+app.controller('weatherController', ['$scope', '$timeout', 'weatherService',
+	function ($scope, $timeout, weatherService) {
 		'use strict';
 
-		function fetchWeather(location) {
-			localStorage.setItem('location', location);
-			weatherService.getWeather(location).then(function (data) {
-				$scope.place = data;
-				// Fix 0-9 leading 0 to match images
-				var codes = $scope.place.item.condition.code;
-				if (parseInt($scope.place.item.condition.code) < 10) {
-					code = "0" + code;
-				}
-				codes = $scope.place.item.forecast;
-				for (var i = 0; i < codes.length; i++) {
-					if (codes[i].code < 10) {
-						codes[i].code = "0" + codes[i].code;
+		// Fill the scope variable with weather data
+		function fillWeatherInfo(weatherData) {
+			// Just asign to scope
+			$scope.place = weatherData;
+			$scope.weatherVisible = true;
+		}
+
+		// Ask for weather info and sanitize response
+		$scope.fetchWeather = function (location, fullForecast) {
+			console.log("retrieving weather");
+			if (location === null || location === undefined || location === "") {
+				return;
+			}
+
+			weatherService.getWeather(location).then(
+				// DEFER SUCCESS
+				function (response) { // data is $scope.place 
+					var data = null;
+					if (response === null || response === undefined) {
+						console.log("Cannot retrieve weather, response is null");
+						return;
 					}
-				}
-			});
+					else {
+						data = response.channel;
+					}
+					// Prepare data
+					//$scope.place = data;
+					// Fix 0-9 leading 0 to match images
+					var cod = data.item.condition.code;
+					if (parseInt(cod) < 10) {
+						cod = "0" + cod;
+					}
+					data.item.condition.code = cod;
+					// Also for forecast
+					if (fullForecast) {
+						cod = data.item.forecast;
+					}
+					else {
+						cod = data.item.forecast.slice(0, 5);
+					}
+					for (var i = 0; i < cod.length; i++) {
+						if (cod[i].code < 10) {
+							cod[i].code = "0" + cod[i].code;
+						}
+					}
+					data.item.forecast = cod;
+					localStorage.setItem('weatherData', JSON.stringify(data));
+					fillWeatherInfo(data);
+				},
+				// DEFER FAILED
+				function (reason) {
+					console.error("getWeather deferred promise reject: " + reason);
+				});
 		}
-		// We want to get the weather as soon the controller is ready
-		var location = localStorage.getItem('location');
-		if (location === null) {
-			location = 'Madrid, Spain'; // Default location
+		// !-- fetchWeather --
+
+		// INITIALIZE
+		// Scope variable for ng-show property in #weather div
+		$scope.weatherVisible = false;
+
+		$scope.location = localStorage.getItem('location');
+		if ($scope.location === null) {
+			$scope.location = "Madrid, Spain";
+			localStorage.setItem("location", $scope.location);
 		}
-		fetchWeather(location);
-		// Add new scope variable that we will attach to a button in the view
-		$scope.findWeather = function (location) {
-			// Clear info and fetch weather
-			$scope.place = '';
-			fetchWeather(location);
+		else if ($scope.location === undefined || $scope.location.trim() === "") {
+			$scope.location = "Madrid, Spain"; // Set default
+			localStorage.setItem("location", $scope.location);
+		}
+		$scope.fullForecast = localStorage.getItem('fullForecast');
+		if ($scope.fullForecast === null || $scope.fullForecast === undefined) {
+			$scope.fullForecast = false; // Set default
+			localStorage.setItem("fullForecast", false);
+		}
+		$scope.fullForecast = ($scope.fullForecast == "true"); // To Boolean
+		var weatherData = localStorage.getItem('weatherData');
+		// Fill previous weather data before trying to fetch new info
+		if (weatherData !== null && weatherData !== undefined) {
+			weatherData = JSON.parse(weatherData);
+			fillWeatherInfo(weatherData);
+		}
+		document.getElementById("full_forecast").checked = $scope.fullForecast;
+
+		// Fetch weather info
+		$scope.fetchWeather($scope.location, $scope.fullForecast);
+
+		// Show page content after few milliseconds
+		// enough to let controllers to initialize data
+		document.getElementById("body").style.opacity = 1;
+
+
+		// SAVE SETTINGS
+		$scope.saveSettings = function (loc, fullFo) {
+			if (loc !== undefined && loc.trim() !== "") {
+				localStorage.setItem('location', loc);
+				$scope.location = loc;
+			}
+			if (fullFo === undefined) {
+				fullFo = false;
+			}
+			localStorage.setItem('fullForecast', fullFo);
+			$scope.fullForecast = fullFo;
+
+			$scope.fetchWeather($scope.location, $scope.fullForecast);
 		};
 	}
 ]);
 
 function timeCtrl($scope, $timeout) {
 	'use strict';
-	$scope.clock = "loading clock..."; // initialise the time variable
+	$scope.clock = Date.now(); // initialise the time variable
 	$scope.tickInterval = 1000; //ms
 	var tick = function () {
 		$scope.clock = Date.now(); // get the current time
